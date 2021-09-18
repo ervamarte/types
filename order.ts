b497cdbd6b48cbb872b2d5318a3ac80684f0d00b
William Novak <williamnvk@gmail.com>
2021-09-18 10:20:56 -0300

    feat(order): add order types

---
+++ order.ts
@@ -0,0 +1,27 @@
+import { Cart } from './cart'
+import { Coupon } from './coupon'
+import { ID, Slug } from './generic'
+import { User } from './user'
+
+export type OrderStatusEnum = 'waiting_payment' | 'paid' | 'canceled' | 'reversed'
+
+export interface Order {
+  id: ID
+  createdAt: Date
+  slug: Slug
+  customer: User
+  coupon?: Coupon
+  cart: Cart
+  subTotal: number
+  discount: number
+  addition: number
+  total: number
+  status: OrderStatus['status']
+}
+
+export interface OrderStatus {
+  id: ID
+  order: Order
+  status: OrderStatusEnum
+  createdAt: Date
+}
