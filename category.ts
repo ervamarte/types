72008f26a8b796e96e6b6a0b4928185f61c2393f
William Novak <williamnvk@gmail.com>
2021-10-17 10:06:58 -0300

    fix(category): add taxonomy

--- category.ts
+++ category.ts
@@ -14,3 +14,20 @@ export interface Category {
   category?: Category
   seo: CategorySEO
 }
+
+export interface Taxonomy {
+  id: Category['id']
+  name: string
+  slug: Category['slug']
+  categories?: 
+    Array<{
+      id: Category['id']
+      name: string
+      slug: Category['slug']
+      subCategories?: Array<{
+        id: Category['id']
+        name: string
+        slug: Category['slug']
+      }>
+    }>
+}
